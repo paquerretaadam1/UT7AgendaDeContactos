8067dd4af07eabef0ba2a644f863ff9f864f0415
dsenaldam1 <dsenaldam1@gmail.com>
2021-04-10 20:25:57 +0200

    Correcion respecto a las clases añadidas a los paquetes

--- src/ut7/agenda/modelo/Personal.java
+++ src/ut7/agenda/modelo/Personal.java
@@ -1,9 +1,27 @@
 package ut7.agenda.modelo;
+import java.time.LocalDate;
 
-public class Personal {
 
-	public Personal() {
+public class Personal extends Contacto {
 
+	private LocalDate dia;
+	private Relacion relacion;
+	
+	/**
+	 * Constructor
+	 */
+	public Personal(String nombre, String apellidos, String telefono,
+			String email, LocalDate dia, Relacion relacion) {
+		super(nombre, apellidos, telefono, email);
+		this.dia = dia;
+		this.relacion = relacion;
 	}
+	public Relacion getRelacion() {
+		return relacion;
+	}
+	public LocalDate getDia() {
+		return dia;
+	}
+	
 
 }
