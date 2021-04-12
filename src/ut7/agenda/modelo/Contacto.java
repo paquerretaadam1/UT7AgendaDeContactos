d8d1e1b1f227645a0b5d5a306a583c38318c632b
paquerretaadam1 <paquerretaadam1@gmail.com>
2021-04-12 18:18:39 +0200

    Clase Contacto terminada

--- src/ut7/agenda/modelo/Contacto.java
+++ src/ut7/agenda/modelo/Contacto.java
@@ -1,13 +1,12 @@
 package ut7.agenda.modelo;
 
-public class Contacto {
+public abstract class Contacto implements Comparable<Contacto> {
 	private String nombre;
 	private String apellidos;
 	private String telefono;
 	private String email;
 
-	public Contacto(String nombre, String apellidos, String telefono,
-			String email) {
+	public Contacto(String nombre, String apellidos, String telefono, String email) {
 		this.nombre = nombre.toUpperCase();
 		this.apellidos = apellidos.toUpperCase();
 		this.telefono = telefono;
@@ -52,4 +51,45 @@ public class Contacto {
 
 	}
 
+	/**
+	 * 
+	 * @return primera letra del apellido
+	 */
+	public char getPrimeraLetra() {
+		return this.apellidos.charAt(0);
+	}
+
+	public boolean equals(Contacto c) {
+		if (c == null) {
+			return false;
+		}
+		if (c == this) {
+			return true;
+		}
+		if (c.getClass() != this.getClass()) {
+			return false;
+		}
+		return c.getApellidos().equalsIgnoreCase(this.getApellidos())
+				&& c.getNombre().equalsIgnoreCase(this.getApellidos())
+				&& c.getEmail().equalsIgnoreCase(this.getEmail());
+	}
+
+	public int compareTo(Contacto c) {
+		int resul = this.getApellidos().compareTo(c.getApellidos());
+		if (resul != 0) {
+			return resul;
+		}
+		return this.getNombre().compareTo(c.getNombre());
+
+	}
+
+	public abstract String Firmar();
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(this.getApellidos() + ",");
+		sb.append(this.getNombre() + "(" + this.getClass().getSimpleName() + ")\n");
+		sb.append("Tlfn: " + this.getTelefono() + " |  " + "email" + this.getEmail());
+		return sb.toString();
+	}
 }
