a84b0594d7fbcfd254045f04c006801d66c298b9
dsenaldam1 <dsenaldam1@gmail.com>
2021-04-12 18:48:53 +0200

    Añadido metodo getFirmaEmail, y nuevas correcciones

--- src/ut7/agenda/modelo/Personal.java
+++ src/ut7/agenda/modelo/Personal.java
@@ -1,5 +1,6 @@
 package ut7.agenda.modelo;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 
 public class Personal extends Contacto {
@@ -11,9 +12,9 @@ public class Personal extends Contacto {
 	 * Constructor
 	 */
 	public Personal(String nombre, String apellidos, String telefono,
-			String email, LocalDate dia, Relacion relacion) {
+			String email, String dia, Relacion relacion) {
 		super(nombre, apellidos, telefono, email);
-		this.dia = dia;
+		this.dia = LocalDate.parse(dia, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
 		this.relacion = relacion;
 	}
 	public Relacion getRelacion() {
@@ -27,19 +28,35 @@ public class Personal extends Contacto {
 	 * Devuelve true /false si es el día del cumpleaños o no
 	 *  
 	 */
-	public  boolean esCumpleaños(Contacto otro)  {
-		return this.dia.equals(((Personal) otro).getDia());
-	
-		/**
-		 * Representacion textual de contactos personales
-		 *  
-		 */
+	public boolean esCumpleaños(Contacto otro) {
+		if (otro == null) {
+			return false;
+		}
+		if (this == otro) {
+			return true;
+		}
+		if (this.getClass() != otro.getClass()) {
+			return false;
+		}
+		return this.dia.getMonth().equals(((Personal) otro).getDia().getMonth()) && 
+				(this.dia.getDayOfMonth() == ((Personal) otro).getDia().getDayOfMonth());
 	}
+	
+	/**
+	 * Representacion textual de contactos personales
+	 *  
+	 */
+	
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append(super.toString());
-		sb.append("Fecha de nacimiento: " + dia + "\n");
+		sb.append("\n" +"Fecha de nacimiento: " + dia + "\n");
 		sb.append("Relacion: " + relacion + "\n");
 		return sb.toString();
 	}
+	public String getFirmaEmail() {
+		return "Un abrazo!!";
+				
+	}
+	
 }
