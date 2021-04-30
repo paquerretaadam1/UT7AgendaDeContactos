fbc647469310131b823165e387a06c39315e238d
paquerretaadam1 <paquerretaadam1@gmail.com>
2021-04-30 20:07:52 +0200

    Cambiado nombre de paquetes

--- src/agenda/io/AgendaIO.java
+++ src/agenda/io/AgendaIO.java
@@ -1,4 +1,4 @@
-package ut7.agenda.io;
+package agenda.io;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -12,11 +12,11 @@ import java.time.format.DateTimeParseException;
 import java.util.List;
 import java.util.Map;
 
-import ut7.agenda.modelo.AgendaContactos;
-import ut7.agenda.modelo.Contacto;
-import ut7.agenda.modelo.Personal;
-import ut7.agenda.modelo.Profesional;
-import ut7.agenda.modelo.Relacion;
+import agenda.modelo.AgendaContactos;
+import agenda.modelo.Contacto;
+import agenda.modelo.Personal;
+import agenda.modelo.Profesional;
+import agenda.modelo.Relacion;
 
 /**
  * Utilidades para cargar la agenda
